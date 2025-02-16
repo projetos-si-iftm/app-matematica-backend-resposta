ef3676ccd64e3ee2b4338ce1c883bf25207f1938
fromanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-02-16 15:58:36 -0300

    refactor: mudanca do idCategoria

--- src/main/java/br/edu/iftm/app_ensino_matematica_backend_resposta/repository/RodadaRepository.java
+++ src/main/java/br/edu/iftm/app_ensino_matematica_backend_resposta/repository/RodadaRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import br.edu.iftm.app_ensino_matematica_backend_resposta.model.Rodada;
 
 public interface RodadaRepository extends MongoRepository<Rodada, UUID> {
-    List<Rodada> findByIdCategoriaAndDificuldade(UUID id_categoria, int dificuldade);
+    List<Rodada> findByIdCategoriaAndDificuldade(UUID idCategoria, int dificuldade);
     List<Rodada> findByIdCategoriaAndIdAluno(UUID idCategoria, UUID idAluno);
 }
