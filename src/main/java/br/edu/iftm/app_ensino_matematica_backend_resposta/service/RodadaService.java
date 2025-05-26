3a821927fe2a0b633f57cfd4a5f9de9bdf075496
fromanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-05-26 17:49:29 -0300

    feat: salva rodada com maior pontuacao

--- src/main/java/br/edu/iftm/app_ensino_matematica_backend_resposta/service/RodadaService.java
+++ src/main/java/br/edu/iftm/app_ensino_matematica_backend_resposta/service/RodadaService.java
@@ -4,21 +4,23 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import br.edu.iftm.app_ensino_matematica_backend_resposta.model.DTO.RodadaDTO;
 import br.edu.iftm.app_ensino_matematica_backend_resposta.model.Resposta;
 import br.edu.iftm.app_ensino_matematica_backend_resposta.model.Rodada;
-import br.edu.iftm.app_ensino_matematica_backend_resposta.model.DTO.RodadaDTO;
 import br.edu.iftm.app_ensino_matematica_backend_resposta.repository.RodadaRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class RodadaService {
-    private final RodadaRepository rodadaRepository;
 
+    private final RodadaRepository rodadaRepository;
+    private final RestTemplate restTemplate = new RestTemplate();
 
     public Rodada getRodadaById(UUID id_rodada) {
-        
+
         return rodadaRepository.findById(id_rodada).orElse(null);
 
     }
@@ -32,14 +34,27 @@ public class RodadaService {
     }
 
     public RodadaDTO saveRodada(RodadaDTO rodadaDTO, List<Resposta> respostas) {
+        // Busca a rodada com maior pontuação do aluno
+        Rodada rodadaAnterior = rodadaRepository.findTopByIdAlunoOrderByPontuacaoDesc(rodadaDTO.getIdAluno()).orElse(null);
+
+        // Se existe uma rodada anterior e a pontuação atual não é maior, não salva
+        if (rodadaAnterior != null && rodadaDTO.getPontuacao() <= rodadaAnterior.getPontuacao()) {
+            System.out.println("rodada anterior maior que atual"); // Ou lance uma exceção, ou retorne uma mensagem de erro, conforme sua regra de negócio
+        }
+
         Rodada rodada = new Rodada();
-        rodada.setId_rodada(UUID.randomUUID()); // Gera um UUID aleatório para id_rodada
+        rodada.setId_rodada(UUID.randomUUID());
         rodada.setIdCategoria(rodadaDTO.getIdCategoria());
         rodada.setIdAluno(rodadaDTO.getIdAluno());
         rodada.setDificuldade(rodadaDTO.getDificuldade());
         rodada.setPontuacao(rodadaDTO.getPontuacao());
-        rodada.setRespostas(respostas); // Adiciona a lista de respostas
+        rodada.setRespostas(respostas);
         rodada = rodadaRepository.save(rodada);
+
+        // if (rodadaAnterior != null && rodadaAnterior.getPontuacao() < 60 && rodada.getPontuacao() >= 60 || rodadaAnterior == null && rodada.getPontuacao() > 60) {
+        //     String url = "http://localhost:3004/manage/question";
+        //     restTemplate.postForObject(url, null, String.class);
+        // }
         return RodadaDTO.convert(rodada);
     }
 
